dfe9cab853b9f60eb3c5f5aa42a9aec3a01e5444
Victor255 <tagbar95@gmail.com>
2015-12-04 13:56:11 -0600

    with Unittest

--- unitests.py
+++ unitests.py
@@ -6,6 +6,7 @@ class FactorialTest(unittest.TestCase):
     def test_factorial(self):
         self.assertEqual(application.factorial(5), 120)
         self.assertEqual(application.factorial(2), 2)
+        self.assertEqual(application.factorial(7), 5040)
 
 if __name__ == '__main__':
     unittest.main()
