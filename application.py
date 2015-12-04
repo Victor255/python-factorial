0e40dee196779f5b60d2405ed3e70dcea33c07ca
Victor255 <tagbar95@gmail.com>
2015-12-04 13:29:51 -0600

    documented functions

--- application.py
+++ application.py
@@ -4,41 +4,39 @@ import os
 import sys
 
 def clear():
-    """This cleans the screen"""
-    if os.name == ("posix"): 
+    """THIS CLEANS THE SCREEEN"""
+    if os.name == ("posix"):
         os.system("clear")
     elif os.name == ("ce", "nt", "dos"):
         os.system("cls")
 clear()
 
 def factorial(number):
-    """This creates the factorial of the any number"""
+    """THIS CREATES THE FACTORIAL OF ANY NUMBER"""
     if number <= 1:
         return 1
     return number * factorial(number-1)
 
 def valid_number():
-    """This verifies if the user inserted numbers"""
+    """THIS VERIFIES IF THE USER INSERTED NUMBERS"""
     clear()
     while True:
-        number = raw_input("Insert the number: \n")
+        number = raw_input("Insert the number: ")
         try:
             number = int(number)
             return number
         except ValueError:
-            clear()
-            print "Insert only numbers please"
+            print "Insert only numbers please\n"
     return number
 
 def print_factorial():
-    """This ask the number to show the factorial"""
+    """THIS ASK THE NUMBER TO SHOW THE FACTORIAL"""
     number = valid_number()
     factorial_number = str(factorial(number))
-    clear()
     print "\nThe factorial of %d is %s " % (number, factorial_number)
 
 def other_factorial():
-    """This ask is want to see other number"""
+    """THIS ASK IS WANT TO SEE OTHER NUMBER"""
     print_factorial()
     while True:
         ask_user = raw_input("\nDo you want to know the factorial of other number?  y/n  ")
@@ -48,13 +46,12 @@ def other_factorial():
             clear()
             menu()
         else:
-            clear()
-            print "Only can write -y- or -n-\n"
+            print "Insert Only y/n Please"
 
 def menu():
-    """This saves the menu"""
-    print "Factorial of a number"
-    print "Exit"
+    """THIS SHOWS THE MENU"""
+    print "(1)Factorial of a number"
+    print "(2)Exit"
     while True:
         ask_user = raw_input(" -- ")
         if ask_user == "1" or ask_user == "factorial":
@@ -63,9 +60,7 @@ def menu():
             clear()
             sys.exit()
         else:
-            clear()
-            print "Invalidate Election\n"
-            menu()
+            print "\nInvalid Option"
 
 if __name__ == '__main__':
     menu()
